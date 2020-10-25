c69fc11138c510997fb1a5d1c0061d1fa7b43872
Chad Stearns <chad@humio.com>
2020-10-25 15:48:59 -0400

    Take ip address as argument

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::env;
+use std::process;
 
 async fn welcome() -> impl Responder {
     HttpResponse::Ok().body("Welcome to Chadtech.us")
@@ -6,11 +8,30 @@ async fn welcome() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(welcome))
-    })
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+    let args: Vec<String> = env::args().collect();
+
+    match args.get(1) {
+        None => {
+            eprintln!("You must provide an argument for Chadtechus, if you are developing locally try `cargo run dev`");
+            process::exit(0x0100);
+        }
+        Some(arg) => {
+
+            let ip_address = if arg == "dev" {
+                "127.0.0.1:8080"
+            } else {
+                arg
+            };
+
+            HttpServer::new(|| {
+                App::new()
+                    .route("/", web::get().to(welcome))
+            })
+                .bind(ip_address)?
+                .run()
+                .await
+        }
+    }
+
+
 }
